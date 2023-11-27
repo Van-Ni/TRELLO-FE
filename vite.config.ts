116d853239195aa9ae0c4852fd45b5d9b094c06c
Van Ni <vanni4018@gmail.com>
2023-11-27 08:52:44 +0700

    [#3] Importing SVG files as React components with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import svgr from "vite-plugin-svgr";
 // #Trello3 
 // https://vitejs.dev/config/
 //https://stackoverflow.com/questions/68241263/absolute-path-not-working-in-vite-project-react-ts
 //https://dev.to/willochs316/how-to-fix-absolute-path-not-working-in-vite-project-react-ts-3aj9
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),svgr()],
   resolve: {
     alias: [
       { find: '~', replacement: '/src' },
       { find: '@components', replacement: '/src/components' },
       { find: '@pages', replacement: '/src/pages' },
       { find: '@utils', replacement: '/src/utils' },
+      { find: '@assets', replacement: '/src/assets' },
     ],
    },
 })
